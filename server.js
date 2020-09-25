4479a68a4a76de35d1e7b35303dea3afe4e1ff24
Sophia Castillo <Sphcastillo@gmail.com>
2020-09-24 17:20:45 -0700

    MERN

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded( {extended: true} ));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
