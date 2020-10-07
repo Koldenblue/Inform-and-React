d812013be2ec946e263f0731d5baccd0cf68b5dd
Kevin Miller <kmillergit@outlook.com>
2020-10-07 11:17:35 -0700

    added shadows to posters. Unnecessary css has been commented out already

--- client/src/components/Posters.js
+++ client/src/components/Posters.js
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "react-bootstrap/Image";
 
 
 function Posters() {
+  const [zoomA, setZoomA] = useState(false)
+
   let styles = {
-    posterImages: {
-      maxWidth: '100%'
-    },
     containerDiv: {
       marginTop: '2em',
       marginBottom: '2em',
     }
   }
+
+
+
   return (
     <div class='container-fluid' style={styles.containerDiv}>
       <div className='row' >
         <div className='col-md-4'>
-          <Image src={require('../assets/WHATTOBRING.png')} style={styles.posterImages} />
+          <Image src={require('../assets/WHATTOBRING.png')} className='poster-images' />
         </div>
         <div className='col-md-4'>
-          <Image src={require('../assets/ELECTIONDAY_CHECKLIST.png')} style={styles.posterImages} />
+          <Image src={require('../assets/ELECTIONDAY_CHECKLIST.png')} className='poster-images' />
         </div>
         <div className='col-md-4'>
-          <Image src={require('../assets/NEVADA.png')} style={styles.posterImages} />
+          <Image src={require('../assets/NEVADA.png')} className='poster-images' />
         </div>
       </div>
+
     </div>
   )
 }
